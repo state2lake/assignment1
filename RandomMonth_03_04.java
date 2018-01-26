3424438321b41158549fc759cf1591429cbbc2e9
state2lake <34748435+state2lake@users.noreply.github.com>
2018-01-26 14:38:01 -0500

    Update RandomMonth_03_04.java

--- RandomMonth_03_04.java
+++ RandomMonth_03_04.java
@@ -3,46 +3,25 @@ import java.util.Random;
 public class RandomMonth_03_04 {
 
 	public static void main(String[] args) {
-	String [] month = new String[] {"January","February","March", "April", "May","June","July","August","September","October","November","December"};
-	Random rnd = new Random();
-	int monthInt = rnd.nextInt(12)+1;
-	System.out.println(monthInt +  "\n");
+	nt month = (int)((Math.random() * 12) + 1);
+		System.out.println(month + "\n");
+		
+		switch (month)
+		{
+			case 1: System.out.println("January"); break;
+			case 2: System.out.println("February"); break;
+			case 3: System.out.println("March"); break;
+			case 4: System.out.println("April"); break;
+			case 5: System.out.println("May"); break;
+			case 6: System.out.println("June"); break;
+			case 7: System.out.println("July"); break;
+			case 8: System.out.println("August"); break;
+			case 9: System.out.println("September"); break;
+			case 10: System.out.println("October"); break;
+			case 11: System.out.println("November"); break;
+			case 12: System.out.println("December");
 	
-	if (monthInt == 1) {
-		System.out.println("January");
-	}
-	else if(monthInt == 2) {
-		System.out.println("February");
-	}
-	else if(monthInt == 3) {
-		System.out.println("March");
-	}
-	else if(monthInt == 4) {
-		System.out.println("April");
-	}
-	else if (monthInt == 5) {
-		System.out.println("May");
-	}
-	else if (monthInt == 6) {
-		System.out.println("June");
-	}
-	else if (monthInt == 7) {
-		System.out.println("July");
-	}
-	else if (monthInt == 8) {
-		System.out.println("August");
-	}
-	else if (monthInt == 9) {
-		System.out.println("September");
-	}
-	else if (monthInt == 10) {
-		System.out.println("October");
-	}
-	else if (monthInt == 11) {
-		System.out.println("November");
-	}
-	else {
-		System.out.println("December");
+	
 	}
 	}
 }
